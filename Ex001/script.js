6ae0d4f3403c72bc2dc1089ee74cba9cd3fb9d20
Pedro dos Santos <122922623+pedrosantosgithub@users.noreply.github.com>
2023-05-09 10:25:49 -0300

    Ex01 finalizado

--- Ex001/script.js
+++ Ex001/script.js
@@ -1,6 +1,5 @@
 const msg = document.querySelector('#msg')
 const img = document.querySelector('#imagem')
-
 let date = new Date()
 let hours = date.getHours()
 let minutes = date.getMinutes()
@@ -9,15 +8,19 @@ console.log(minutes)
 msg.innerText = `Agora são: ${hours}:${minutes}`
 function carregar(){
     if (hours >= 0 && hours < 12){
-        img.src ='./5.png';
-        document.body.style.background = '#0000'
-        document.body.style.boxShadow = '3px 3px 10px rgba(39, 65, 214, 0.26)'
+        img.src ='./7.png';
+        document.body.style.background = '0BA7DB';
+        var section = document.querySelector('section');
+        section.style.boxShadow = '5px 5px 1000px #2596be';
     } else if (hours >= 12 && hours <= 18){
         img.src = './17.png'
-        document.body.style.background = '#F5500F'
-        document.body.classList.add('sombra')
+        document.body.style.background = '#F5500F';
+        var section = document.querySelector('section');
+        section.style.boxShadow = '5px 5px 100px #d85c81';
     } else{
         img.src = './19.png'
+        var section = document.querySelector('section');
+        section.style.boxShadow = '5px 5px 100px #77d1ef';
     }
 }
 
